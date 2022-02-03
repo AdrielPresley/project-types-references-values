1d40173c8b83cde0104063104b298e7cb2149e15
AdrielPresley <adriel_tel11@hotmail.com>
2022-02-03 15:44:18 -0300

    Update

---
+++ Exe4/Program.cs
@@ -0,0 +1,21 @@
+﻿using static System.Console;
+Numero x = new Numero(2);
+Numero y = new Numero(2);
+
+if(x == y)
+{
+  WriteLine($"x e y são iguais");
+}
+else
+{
+  WriteLine("x e y são diferentes");
+}
+
+class Numero
+{
+    public int N { get; set; }
+    public Numero(int n)
+    {
+        N = n;
+    }
+}

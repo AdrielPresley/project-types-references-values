eee52acff709479b2b08abb7d6fcfeace9f82734
AdrielPresley <adriel_tel11@hotmail.com>
2022-02-03 14:10:46 -0300

    Update

---
+++ Exe3/Program.cs
@@ -0,0 +1,16 @@
+﻿using static System.Console;
+
+Pessoa p1 = new Pessoa();
+
+p1.Nome = "Jean";
+p1.Idade = 25;
+
+p1.EnderecoPessoa = new Endereco()
+{
+  Logradouro = "Rua Chaves Eldorado",
+  Numero = 221,
+  CEP = "45698",
+  Cidade = "São Paulo",
+};
+
+WriteLine("Fim");
